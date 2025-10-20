067e70520fb82888bea8117539eda4dadf4c1b5c
Cristobal E. <cristobalelizarraraz905@gmail.com>
2025-10-19 21:23:29 -0700

    added about me text

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -9,8 +9,15 @@ export default function AboutMe() {
           <h1>{"About Me"}</h1>
           <h6>{`Hi, I’m Cristobal Elizarraraz, welcome to my portfolio! 
               I am a recent Computer Science May 2025 graduate from CSU-Monterey Bay, 
-              driven by a passion for building reliable and scalable software systems, 
-              and a talent for creative storytelling.`}</h6>
+              driven by a passion for building reliable, scalable, and maintainable software systems, 
+              and a talent for creative storytelling. I bring a strong blend of software development expertise, 
+              collaborative project experience, and user-focused design skills to teams where adaptability, and problem-solving drive success.`}
+              <br/><br/>
+              {`During my academic studies, I’ve worked on a variety of collaborative development projects that strengthened both my technical and teamwork skills. 
+              From building full-stack web applications with React, Flask, and Spring Boot to developing interactive 3D games in Unity, I’ve gained hands-on experience across multiple programming languages and frameworks. 
+              These projects taught me how to design user-focused interfaces, debug and deploy reliable code, and adapt quickly to new tools and technologies. 
+              Through internships and academic collaborations, I’ve learned to contribute effectively in agile environments, by balancing independent problem-solving with clear communication and shared goals.`}</h6>
+              <br/><br/>
         </div>
         <Image
           src="/professionalPhoto.JPG"
