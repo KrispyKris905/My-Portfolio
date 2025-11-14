5e9f1ae7e82cefe4d9cee6681b5c5afa91324b53
Cristobal E. <cristobalelizarraraz905@gmail.com>
2025-11-13 16:51:46 -0800

    Added album cover image and modified recently played component styling.

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { ContentSection } from "@/components/content-section";
+import RecentlyPlayed from '@/components/RecentlyPlayedSpotify';
+
 
 export default function AboutMe() {
   return (
@@ -7,17 +9,18 @@ export default function AboutMe() {
       <ContentSection>
         <div>
           <h1>{"About Me"}</h1>
-          <h6>{`Hi, I’m Cristobal Elizarraraz, welcome to my portfolio! 
-              I am a recent Computer Science May 2025 graduate from CSU-Monterey Bay, 
-              driven by a passion for building reliable, scalable, and maintainable software systems, 
-              and a talent for creative storytelling. I bring a strong blend of software development expertise, 
-              collaborative project experience, and user-focused design skills to teams where adaptability, and problem-solving drive success.`}
-              <br/><br/>
-              {`During my academic studies, I’ve worked on a variety of collaborative development projects that strengthened both my technical and teamwork skills. 
-              From building full-stack web applications with React, Flask, and Spring Boot to developing interactive 3D games in Unity, I’ve gained hands-on experience across multiple programming languages and frameworks. 
-              These projects taught me how to design user-focused interfaces, debug and deploy reliable code, and adapt quickly to new tools and technologies. 
-              Through internships and academic collaborations, I’ve learned to contribute effectively in agile environments, by balancing independent problem-solving with clear communication and shared goals.`}</h6>
-              <br/><br/>
+          <p>{`Hi, I’m Cristobal Elizarraraz, welcome to my portfolio! 
+            I am a recent Computer Science May 2025 graduate from CSU-Monterey Bay, 
+            driven by a passion for building reliable, scalable, and maintainable software systems, 
+            and a talent for creative storytelling. I bring a strong blend of software development expertise, 
+            collaborative project experience, and user-focused design skills to teams where adaptability, and problem-solving drive success.`}
+            <br/><br/>
+            {`During my academic studies, I’ve worked on a variety of collaborative development projects that strengthened both my technical and teamwork skills. 
+            From building full-stack web applications with React, Flask, and Spring Boot to developing interactive 3D games in Unity, I’ve gained hands-on experience across multiple programming languages and frameworks. 
+            These projects taught me how to design user-focused interfaces, debug and deploy reliable code, and adapt quickly to new tools and technologies. 
+            Through internships and academic collaborations, I’ve learned to contribute effectively in agile environments, by balancing independent problem-solving with clear communication and shared goals.`}
+          </p>
+          <br/><br/>
         </div>
         <Image
           src="/professionalPhoto.JPG"
@@ -29,7 +32,7 @@ export default function AboutMe() {
       </ContentSection>
 
       <ContentSection id="alt">
-        {'alternative theme test'}
+        <RecentlyPlayed />
       </ContentSection>
     </div>
   );
--- components/RecentlyPlayedSpotify/index.tsx
+++ components/RecentlyPlayedSpotify/index.tsx
@@ -1,18 +1,29 @@
 import { getRecentlyPlayed } from '@/lib/spotify';
+import Image from 'next/image';
+import { ContentSection } from "@/components/content-section";
+
 
 export default async function RecentlyPlayedSpotify() {
   const data = await getRecentlyPlayed(5);
 
   return (
-    <div>
-      <h2>Recently Played</h2>
-      <ul>
+    <div className="w-full">
+      <h4>Recently Played on Spotify</h4>
+      <div className="responsive-flex ">
         {data.items?.map((item: any) => (
-          <li key={item.played_at}>
-            {item.track.name} - {item.track.artists[0].name}
-          </li>
+          <div key={item.track.id} className="flex flex-col items-center">
+            <Image
+              src={item.track.album.images[0]?.url || '/placeholder.png'}
+              alt={`${item.track.name} album cover`}
+              width={150}
+              height={150}
+              className="img-square"
+            />
+            <h6>{item.track.name}</h6>
+            <h6>{item.track.artists[0].name}</h6>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
